3d0843b19180e1e40869749928259e4872ad55c6
Marijn Doeve <marijn@doeve.me>
2021-11-24 12:47:05 +0100

    Initial Commit

---
+++ Shared/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Shared
+//
+//  Created by Marijn Doeve on 24-11-2021.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
